93101a146b09f5f0aadfa47978863c62b2628a5c
Thiago Auler <thiagoauler@users.noreply.github.com>
2022-02-23 19:30:14 -0300

    Update name2hash.sh
    Now nes roms are named a little different:
    [unhedered_hash].[headered_hash].rom

--- name2hash.sh
+++ name2hash.sh
@@ -2,30 +2,35 @@
 
 for directory in MiSTer/*
 do
-   echo
-   echo Processing new system
-   echo ... $directory ...
-   for filename in "$directory"/*
-   do
-      header=$(hexdump -e '16/1 "%02x"' -n 4 "$filename")
-      if [ $header == 4e45531a ]
-      then
-         filehash=$(tail -c +17 "$filename" | md5 -q)
-      else
-         filehash=$(md5 -q "$filename")
-      fi
-      newfilename="$directory"/"$filehash".rom
-
-      if [ "$filename" != "$newfilename" ]
-      then
-         if test -f "$newfilename"
+   if test -d "$directory"
+   then
+      echo
+      echo Processing new system
+      echo ... $directory ...
+      skipped=0
+      for filename in "$directory"/*
+      do
+         header=$(hexdump -e '16/1 "%02x"' -n 4 "$filename")
+         if [ $header == 4e45531a ]
          then
-            echo "Conflict: " $filename
+            filehash=$(tail -c +17 "$filename" | md5 -q)"."$(md5 -q "$filename")
          else
-            mv "$filename" "$newfilename"
+            filehash=$(md5 -q "$filename")
          fi
-      else
-         echo Skip $filename
-      fi
-   done
+         newfilename="$directory"/"$filehash".rom
+
+         if [ "$filename" != "$newfilename" ]
+         then
+            if test -f "$newfilename"
+            then
+               echo "Conflict: " $filename
+            else
+               mv "$filename" "$newfilename"
+            fi
+         else
+            skipped=$((skipped + 1))
+         fi
+      done
+      echo Skipped $skipped files.
+   fi
 done
